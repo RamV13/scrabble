74284c6c6c6228f4908ffe0733fe4a0a645810b9
Ubuntu <ubuntu@csvm>
2016-11-02 21:46:07 -0400

    more documentation in dict

--- dictionary.mli
+++ dictionary.mli
@@ -2,6 +2,7 @@
 (*A node is composed of a character and a map from characters to children nodes*)
 type node
 
+(*Basic required trie functionality*)
 val empty : node
 val is_empty : node -> bool
 val find : string -> node -> node
@@ -9,7 +10,11 @@ val add : string -> node -> node
 val remove: string -> node -> node
 val mem: string -> node -> bool
 
-(*optional but i thought might be worth it*)
+
 val is_leaf : string -> bool
+
+(*Return all possible extentions of an input string*)
 val extensions : string -> string list
+
+(*Function which accepts a string and returns a completed trie*)
 val make : string -> node
