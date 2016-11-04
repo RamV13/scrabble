3945c96d975df09951201e555f4bb3bbad761947
Ram Vellanki <ramsai.vellanki@gmail.com>
2016-11-04 03:46:08 -0400

    Updated PORT to 80

--- public/server.js
+++ public/server.js
@@ -1,6 +1,6 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8000, function(){
-    console.log('Server running on 8000...');
+connect().use(serveStatic(__dirname)).listen(80, function(){
+    console.log('Server running on 80...');
 });
 
