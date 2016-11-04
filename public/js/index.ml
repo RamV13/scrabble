a7d9c54203eb518837341324d9ad515286a4d489
ramv13 <ramsai.vellanki@gmail.com>
2016-11-04 04:32:26 -0400

    Added color changing to button handling for testing

--- public/js/index.ml
+++ public/js/index.ml
@@ -8,23 +8,34 @@ let fail = fun () -> assert false
 let get_element_by_id id = 
   Js.Opt.get Dom_html.document##getElementById (Js.string id) fail
 
-(* [handle_btn_join] is the callback to handle the click events of the join 
- * button *)
-let handle_btn_join _ = 
-  Dom_html.window##alert (Js.string "JOIN");
-  Js._true
+let toggle_join = ref true
+let toggle_create = ref true
 
-(* [handle_btn_create] is the callback to handle the click events of the create
- * button *)
-let handle_btn_create _ = 
-  Dom_html.window##alert (Js.string "CREATE");
-  Js._true
+(* [handle_btn_join btn ()] is the callback to handle the click events of the 
+ * join button [btn] *)
+let handle_btn_join btn _ = 
+  if !toggle_join then 
+    btn##style##cssText <- Js.string "background: #FF9800; width: 100px; margin-right: 10px"
+  else 
+    btn##style##cssText <- Js.string "background: #009688; width: 100px; margin-right: 10px";
+  toggle_join := not !toggle_join;
+  Js._false
+
+(* [handle_btn_create btn ()] is the callback to handle the click events of the 
+ * create button [btn] *)
+let handle_btn_create btn _ = 
+  if !toggle_create then 
+    btn##style##cssText <- Js.string "background: #FF9800; width: 100px"
+  else 
+    btn##style##cssText <- Js.string "background: #009688; width: 100px";
+  toggle_create := not !toggle_create;
+  Js._false
 
 let onload _ =
   let btn_join = get_element_by_id "btn_join" in
   let btn_create = get_element_by_id "btn_create" in
-  btn_join##onclick <- Dom_html.handler handle_btn_join;
-  btn_create##onclick <- Dom_html.handler handle_btn_create;
+  btn_join##onclick <- Dom_html.handler (handle_btn_join btn_join);
+  btn_create##onclick <- Dom_html.handler (handle_btn_create btn_create);
   Js._false
 
 let _ = 
