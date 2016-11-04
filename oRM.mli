d7c24c1188d537d8ed6cb5a39e5e2ccf679b7819
ramv13 <ramsai.vellanki@gmail.com>
2016-11-03 21:26:03 -0400

    Converted game id to string in ORM interface

--- oRM.mli
+++ oRM.mli
@@ -3,10 +3,10 @@
 val save_game : Game.state -> unit
 
 (* [get_game id] gets the [game] associated with [id] from the database *)
-val get_game : string -> Game.state
+val get_game : int -> Game.state
 
 (* [get_games ()] gets the list of games from the database *)
 val get_games : unit -> Game.state list
 
 (* [remove_game id] remove the game associated with [id] from the database *)
-val remove_game : string -> unit
+val remove_game : int -> unit
