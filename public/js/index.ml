97028095eeb62ffb8cef2505fdd167cfd4b8ded4
ramv13 <ramsai.vellanki@gmail.com>
2016-11-04 04:18:51 -0400

    Fixed HTML and set up button handling structure

--- public/js/index.ml
+++ public/js/index.ml
@@ -1,15 +1,30 @@
 
 open Dom
 
+(* [fail] is a failure callback *)
+let fail = fun () -> assert false
+
+(* [get_element_by_id id] gets a DOM element by [id] *)
+let get_element_by_id id = 
+  Js.Opt.get Dom_html.document##getElementById (Js.string id) fail
+
+(* [handle_btn_join] is the callback to handle the click events of the join 
+ * button *)
+let handle_btn_join _ = 
+  Dom_html.window##alert (Js.string "JOIN");
+  Js._true
+
+(* [handle_btn_create] is the callback to handle the click events of the create
+ * button *)
+let handle_btn_create _ = 
+  Dom_html.window##alert (Js.string "CREATE");
+  Js._true
+
 let onload _ =
-  let body = Js.Opt.get (Dom_html.document##getElementById (Js.string "body")) (fun () -> assert false) in
-  let createButton ?show:(show=Js._false) ?value:(value=None) title action = 
-    let but = Dom_html.createInput ?_type:(Some (Js.string "submit")) Dom_html.document in
-    but##value <- Js.string title;
-    but##className <- Js.string "mdl-button mdl-js-button mdl-button--raised mdl-button--colored mdl-js-ripple-effect";
-    Dom.appendChild body but;
-  in
-  (* ignore (createButton "Button" "Button"); *)
+  let btn_join = get_element_by_id "btn_join" in
+  let btn_create = get_element_by_id "btn_create" in
+  btn_join##onclick <- Dom_html.handler handle_btn_join;
+  btn_create##onclick <- Dom_html.handler handle_btn_create;
   Js._false
 
 let _ = 
