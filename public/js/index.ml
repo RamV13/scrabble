f01c697520b3643e5efc2a254f6bde2f6f3a1076
ramv13 <ramsai.vellanki@gmail.com>
2016-11-06 01:10:28 -0500

    Implemented eventsource SSE example

--- public/js/index.ml
+++ public/js/index.ml
@@ -13,6 +13,9 @@ let get_element_by_id id =
 let toggle_join = ref true
 let toggle_create = ref true
 
+let event_source_constructor = Js.Unsafe.global##_EventSource
+let event_source = jsnew event_source_constructor (Js.string "http://127.0.0.1:8000")
+
 let test _ = 
   (
   ScrabbleClient.get_game_info ()
@@ -50,6 +53,19 @@ let onload _ =
   let btn_create = get_element_by_id "btn_create" in
   btn_join##onclick <- Dom_html.handler (handle_btn_join btn_join);
   btn_create##onclick <- Dom_html.handler (handle_btn_create btn_create);
+  event_source##onmessage <- Js.wrap_callback (fun e ->
+    (*let json = 
+      e##data
+      |> Js.to_string
+      |> Yojson.Basic.from_string
+    in*)
+    Dom_html.window##alert (e##data);
+    ()
+  );
+  event_source##onerror <- Js.wrap_callback (fun e -> 
+    Dom_html.window##alert (e##data);
+    event_source##close ()
+  );
   Js._false
 
 let _ = 
