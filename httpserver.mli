59826827fb825746a507c4ad1d2adf2adf2bc28c
ramv13 <ramsai.vellanki@gmail.com>
2016-11-06 01:28:07 -0500

    Updated server to include custom server response behavior

--- httpserver.mli
+++ httpserver.mli
@@ -17,12 +17,22 @@ type response = {
   res_body : string
 }
 
-(* [callback] is a function that converts a request into a response *)
+(* [callback] is a function to convert a request into a simple HTTP response *)
 type callback = request -> response
 
-(* [add_route server route callback] adds a route to the server where the 
- * [route] is tied to a callback [callback] *)
+(* [custom_callback] is a function to convert a request into a cohttp response 
+ * which enables further customization of the response mechanism *)
+type custom_callback = request -> (Response.t * Cohttp_lwt_body.t) Lwt.t
+
+(* [add_route (meth,route) callback] adds a route to the server where the 
+ * [route] is tied to a callback [callback] and is served with the HTTP method 
+ * [meth] on the uri [route] *)
 val add_route : meth * string -> callback -> unit
+
+(* [add_custom_route (meth,route) callback] adds a route to the server where the
+ * [route] is tied to the [callback] and is served with the HTTP method [meth]
+ * on the uri [route] *)
+val add_custom_route : Code.meth * string -> custom_callback -> unit
 
 (* [run server] runs a server that listens on an optional port parameter which 
  * defaults to 8000 *)
