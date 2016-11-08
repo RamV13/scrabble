ce0b318c04eeee99d39646abeaab7566650c7e51
Kirk Thaker <kirkthaker66@gmail.com>
2016-11-07 21:11:25 -0500

    Removed word_score

--- ai.mli
+++ ai.mli
@@ -2,7 +2,3 @@
 (* [best_move game player] returns the best move for the given player
  * and current board state. *)
 val best_move : Game.state -> Game.player -> Game.move
-
-(* [word_score word] returns the score for a given word independent of
- * the board being used. *)
-val word_score : string -> int
