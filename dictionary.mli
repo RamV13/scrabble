0f20693f6f406db1b8447f9827c1e288e9ee84df
Kirk Thaker <kirkthaker66@gmail.com>
2016-11-07 21:07:46 -0500

    Reverted module wrapping

--- dictionary.mli
+++ dictionary.mli
@@ -1,22 +1,19 @@
-module type Trie =
-  sig
-    (*A [node] is composed of a character and a map from characters to children nodes*)
-    type t
+(*A [node] is composed of a character and a map from characters to children nodes*)
+type t
 
-    (*Basic required trie functionality*)
-    val empty : t
-    val is_empty : t -> bool
-    val find : string -> t -> t
-    val add : string -> t -> t
-    val remove: string -> t -> t
-    val mem: string -> t -> bool
+(*Basic required trie functionality*)
+val empty : t
+val is_empty : t -> bool
+val find : string -> t -> t
+val add : string -> t -> t
+val remove: string -> t -> t
+val mem: string -> t -> bool
 
-    (*[is_leaf] returns whether or not a given string is fully extended*)
-    val is_leaf : string -> bool
+(*[is_leaf] returns whether or not a given string is fully extended*)
+val is_leaf : string -> bool
 
-    (*[extenstions] returns all possible extentions of an input string*)
-    val extensions : string -> string list
+(*[extenstions] returns all possible extentions of an input string*)
+val extensions : string -> string list
 
-    (*[make] is a function which accepts a string and returns a completed trie*)
-    val make : string -> t
-  end
+(*[make] is a function which accepts a string and returns a completed trie*)
+val make : string -> t
