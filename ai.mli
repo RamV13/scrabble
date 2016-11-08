e4ca113762f4372491fd26d346002ff0b45037ab
Kirk Thaker <kirkthaker66@gmail.com>
2016-11-07 20:29:21 -0500

    Removed board_val

--- ai.mli
+++ ai.mli
@@ -1,12 +1,8 @@
 open Game
 
-(* [board_val game] returns the integer value of the board [game] for the
- * player [player]. *)
-val board_val : Game.state -> Game.player -> int
-
 (* [best_move game player] returns the best move for the given player
  * and current board state. *)
-val best_move : Game.state -> Game.player -> Game.state
+val best_move : Game.state -> Game.player -> Game.move
 
 (* [word_score word] returns the score for a given word independent of
  * the board being used. *)
