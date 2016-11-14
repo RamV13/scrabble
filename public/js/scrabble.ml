d2ccfde821789a9d41d87583fed8cf76200ba5be
ramv13 <ramsai.vellanki@gmail.com>
2016-11-13 23:23:23 -0500

    Implemented callback registration for grid tiles

--- public/js/scrabble.ml
+++ public/js/scrabble.ml
@@ -6,6 +6,9 @@ open Lwt
 (* [board_id] is the HTML id for the board *)
 let board_id = "board"
 
+(* [board_dimension] is the dimension of the board *)
+let board_dimension = 15
+
 (* [fail] is a failure callback *)
 let fail = fun _ -> assert false
 
@@ -17,7 +20,23 @@ let get_element_by_id id =
 let get_tile row col = 
   get_element_by_id ("grid-" ^ string_of_int row ^ "," ^ string_of_int col)
 
+let handle_tile row col _ = 
+  (* TODO *)
+  Js._false
+
+let register_tiles () = 
+  let rec aux row col = 
+    if row >= 0 && col >= 0 then
+      begin
+        (get_tile row col)##onclick <- Dom_html.handler (handle_tile row col);
+        if col = 0 then aux (row - 1) (board_dimension - 1)
+        else aux row (col - 1)
+      end
+  in
+  aux (board_dimension - 1) (board_dimension - 1)
+
 let onload _ =
+  register_tiles ();
   Js._false
 
 let _ = 
