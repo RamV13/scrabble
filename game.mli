9426267b2d57a1e186435b11cb44a3dfabbcc197
briancshi <briancshi@gmail.com>
2016-11-22 01:41:00 -0500

    fix typo in mli

--- game.mli
+++ game.mli
@@ -37,7 +37,7 @@ type move = {
  * be no difference (in the case of adding/removing players or a failed move) or
  * the move given to a state *)
 type diff = {
-  board_diff = ((int * int) * char) list
+  board_diff : ((int * int) * char) list;
   new_turn_val : int;
   players_diff : player list
 }
@@ -56,13 +56,13 @@ val add_player : state -> string -> int
 (* [remove_player state player_id] removes the player with id [player_id] from
  * current game [state], and returns the new state. It replaces the old player
  * with a computer that inherits the removed player's tiles, score, turn, and id
- * raises Failure if there is no player in the game with [player_id] 
+ * asserts false if there is no player in the game with [player_id] 
  * now its mutable and return the new ai player name and its order *)
 val remove_player : state -> string -> (string * int)
 
 (* [execute state move] executes a [move] to produce a new game state from the
  * previous game state [state] 
-* now its mutable raise FailedMove if move failed *)
+ * now its mutable raise FailedMove if move failed *)
 val execute : state -> move -> diff
 
 (* [to_json state] is a json representation of [state] as a string *)
