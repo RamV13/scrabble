53e193784e889c3788796cc875f837b199eaccad
briancshi <briancshi@gmail.com>
2016-11-22 01:07:33 -0500

    implement create_game

--- game.ml
+++ game.ml
@@ -1,5 +1,8 @@
 open Yojson.Basic.Util
 
+exception Full
+exception FailedMove
+
 (* [player] contains the player's identification information, tiles, score,
  * order in the game, and a flag indicating whether this player is an AI *)
 type player = {
@@ -40,33 +43,59 @@ type diff = {
   players_diff : player list
 }
 
-(* [add_player state player_id player_name] adds the player with id [player_id]
- * and name [player_name] to the current game [state], and returns the new state
+let create_bag () = 
+  (* todo this is off *)
+  [
+    'a';'a';'a';'a';'a';'a';'a';'a';'a';
+    'b';'b';
+    'c';'c';
+    'd';'d';'d';'d';
+    'e';'e';'e';'e';'e';'e';'e';'e';'e';'e';'e';'e';
+    'f';'f';
+    'g';'g';'g';
+    'h';'h';
+    'i';'i';'i';'i';'i';'i';'i';'i';'i';
+    'j';
+    'k';'k';
+    'l';'l';'l';'l';
+    'm';'m';
+    'n';'n';'n';'n';'n';'n';
+    'o';'o';'o';'o';'o';'o';'o';'o';
+    'p';'p';
+    'q';
+    'r';'r';'r';'r';'r';'r';
+    's';'s';'s';'s';
+    't';'t';'t';'t';'t';'t';
+    'u';'u';'u';'u';
+    'v';'v';
+    'w';'w';
+    'x';
+    'y';'y';
+    'z';
+    '?';'?'
+  ]
+
+(* [add_player state player_id player_name] adds the player with name 
+ * [player_name] to the current game [state], and returns the turn order that
+ * the player was added to (i.e. the turn of the AI who the player replaced).
  * The player replaces the computer with the lowest order, and inherits its 
  * tiles, score, and turn. 
  * raise Failure if the game is full of players (non computer) already *)
 let add_player s p_n = 
-  let rec get_new_players players acc ai_found =
-    match players with
-    | h::t -> 
-      if h.ai && not ai_found then 
-        let new_player = {h with player_name=p_n; ai=false} in
-        get_new_players t (acc @ [new_player]) true
-      else 
-        get_new_players t (acc @ [h]) ai_found
-    | [] -> 
-      if not ai_found then failwith "Game full"
-      else acc
+  let substituted = 
+      try List.find (fun player -> player.ai) s.players
+      with Not_found -> raise Full
   in
-  let new_players = get_new_players s.players [] false in
-  {s with players=new_players}
+  substituted.player_name <- p_n;
+  substituted.ai <- false;
+  substituted.order
 
 (* [remove_player state player_id] removes the player with id [player_id] from
  * current game [state], and returns the new state. It replaces the old player
  * with a computer that inherits the removed player's tiles, score, turn, and id
  * raises Failure if there is no player in the game with [player_id] *)
-let remove_player s p_id = 
-  let rec get_new_players players acc pl_found =
+let remove_player s p_n = 
+  (* let rec get_new_players players acc pl_found =
     match players with
     | h::t ->
       if h.player_name = p_id then 
@@ -81,10 +110,7 @@ let remove_player s p_id =
   in
   let new_players = get_new_players s.players [] false in
   {s with players=new_players}
-
-(* [get_diff state state] returns the difference [diff] between two game states.
- * requires: The state id and names are equal *)
-let get_diff s1 s2 = 
+  *)
   failwith "unimplemented"
 
 (* [execute state move] executes a [move] to produce a new game state from the 
@@ -121,9 +147,9 @@ let players_to_json players =
 
 (* [to_json state] is a json representation of [state] *)
 let to_json state = 
-  "{\"name\": \"" ^ state.name ^ "\",\"grid\":" ^ (Grid.to_json state.grid) ^ ",\"players\":[" ^ 
-  (players_to_json state.players) ^ "],\"remaining_tiles\": " ^ 
-  (char_list_to_json state.remaining_tiles) ^ ",\"turn\": " ^ 
+  "{\"name\": \"" ^ state.name ^ "\",\"grid\":" ^ (Grid.to_json state.grid) ^ 
+  ",\"players\":[" ^ (players_to_json state.players) ^ "],\"remaining_tiles\": " 
+  ^ (char_list_to_json state.remaining_tiles) ^ ",\"turn\": " ^ 
   (string_of_int state.turn) ^ "}"
 
 let str_to_c s = 
@@ -168,7 +194,90 @@ let from_json json =
     remaining_tiles = r;
     turn = t
   }
-
+(* for testing *)
+(* 
 let _ = 
   to_json { name = "game"; grid = Grid.empty; players = []; remaining_tiles = ['a'; 'b']; turn = 0}
   |> Yojson.Basic.from_string |> from_json |> to_json |> print_endline
+*)
+
+(* COPIED STUFF *)
+
+(* for initializing names to be used in create game *)
+(* [names_file] is the file containing a list of line separated names *)
+let names_file = "names.txt"
+(* [names] is the list of computer names *)
+let names = ref []
+
+(* given bag and n # of tiles to take, return tiles taken and new bag *)
+let take_tiles bag num_to_take = 
+  let take_tile bag index = 
+    let count = ref 0 in
+    let (tile, new_bag) = List.fold_left 
+      (fun (t,b) c -> 
+        if !count = index && t = None then (Some c,b)
+        else (count := !count + 1; (t, c::b)))
+      (None,[]) bag
+    in
+    match tile with 
+    | None -> assert false
+    | Some t -> (t,List.rev new_bag)
+  in
+  Random.self_init ();
+  let rec aux tiles b n =
+    if n = 0 then (tiles,b)
+    else 
+      let (t,bag') = take_tile b (Random.int (List.length b)) in 
+      aux (t::tiles) bag' (n - 1)
+  in
+  aux [] bag num_to_take
+
+let init_names () = 
+  let input_channel = open_in names_file in
+  try
+    let rec process_line () = 
+      let line = input_line input_channel in
+      names := line::!names;
+      process_line ()
+    in
+    ignore (process_line ());
+    close_in input_channel
+  with
+  | End_of_file -> close_in input_channel
+  | exc -> close_in_noerr input_channel; raise exc
+
+let create_game p_n g_n = 
+  let grid = Grid.empty in
+  let bag = create_bag () in
+  let base_player = {player_name=""; tiles=[]; score=0; order=0; ai=true} in
+  let create_ai order bag =
+    let player_name = (List.hd !names) ^ " (AI)" in
+    names := List.tl !names;
+    let (tiles,bag') = take_tiles bag 7 in 
+    ({base_player with player_name; order; tiles}, bag')
+  in
+  let (players,new_bag) = 
+    let rec add_players acc order b = 
+      if order < 4 then 
+        let (new_player,b') = create_ai order b in
+        add_players (new_player::acc) (order + 1) b'
+      else (acc,b)
+    in
+    let (human_tiles,bag') = take_tiles bag 7 in
+    add_players 
+      [{base_player with player_name = p_n; ai=false; tiles = human_tiles}] 
+      1 bag'
+  in
+  {name=g_n; grid; players=List.rev players; remaining_tiles=new_bag; turn=0}
+
+let diff_to_json d = 
+  "hi"
+
+let move_from_json j = 
+  failwith "unimplemented"
+
+let _ = 
+  init_names ();
+  create_game "Brian" "mygame" |> to_json |> print_endline
+
+
