df758ed12e436de2f4b730423070e14155a53d5f
briancshi <briancshi@gmail.com>
2016-11-28 00:20:56 -0500

    add bonus query function

--- grid.ml
+++ grid.ml
@@ -62,6 +62,12 @@ let is_empty board x y = List.nth (List.nth board x) y = None
 
 let get_tile board x y = try (List.nth (List.nth board x) y) with _ -> None
 
+let bonus_letter_at (x,y) = 
+  try List.assoc (x,y) bonus_letter_tiles with _ -> 1
+
+let bonus_word_at (x,y) = 
+  try List.assoc (x,y) bonus_word_tiles with _ -> 1
+
 let rec place_helper1 (row:(char option) list) y c new_row = match row with
 |[] -> new_row
 |h::t -> if y = 0 then place_helper1 t (0-1) c (c::new_row)
