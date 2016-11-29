b8e91b22aa8879b09bb3c6918e0c1f726f7e3e0e
ramv13 <ramsai.vellanki@gmail.com>
2016-11-29 13:35:15 -0500

    Implemented server request logging mechanism

--- server.js
+++ server.js
@@ -3,10 +3,15 @@ var express = require('express');
 var app = module.exports = express();
 
 var proxy = require('http-proxy').createProxyServer({
-  host: 'http://localhost:8000',
+  host: 'http://localhost:8000'
 });
 
 app.use('/api', function(req, res, next) {
+  var data = "";
+  req.on('data', function(chunk) {data += chunk});
+  req.on('end', function() {
+     console.log(req.method + '\t\t' + req.path + '\t\t\t' + data);
+  });
   proxy.web(req, res, {
     target: 'http://localhost:8000/api'
   }, next);
@@ -15,5 +20,5 @@ app.use('/api', function(req, res, next) {
 app.use(express.static(__dirname + '/public'));
 
 app.listen(80, function () {
-  console.log('Listening on port 80...');
+  console.log('----SERVER STARTED---- ' + new Date().toISOString());
 });
