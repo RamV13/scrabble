41980f3022ce63eed6e6b4c1308325e32fb36b2f
briancshi <briancshi@gmail.com>
2016-12-04 03:39:03 -0500

    fix mutability issues with testing, add removeplayertests

--- game_test.ml
+++ game_test.ml
@@ -9,7 +9,7 @@ let tp2 = [((6,7),'a');((6,8),'b');((6,9),'c')]
 let tp3 = [((7,7),'a');((7,8),'b');((7,9),'c');((7,11),'d')]
 let tp4 = [((7,7),'a');((7,8),'b');((7,9),'c');((7,10),'d')]
 
-let p1 = {
+let init_p1 () = {
   player_name = "alan";
   tiles = ['a';'p';'e';'r';'l';'o';'s'];
   score = 0;
@@ -17,7 +17,7 @@ let p1 = {
   ai = false
 }
 
-let p2 = {
+let init_p2 () = {
   player_name = "bob";
   tiles = ['b';'q';'f';'s';'e';'o';'s'];
   score = 0;
@@ -25,7 +25,7 @@ let p2 = {
   ai = false
 }
 
-let p3 = {
+let init_p3 () = {
   player_name = "chris";
   tiles = ['e';'h';'v';'w';'j';'n';'?'];
   score = 0;
@@ -33,7 +33,7 @@ let p3 = {
   ai = true
 }
 
-let p4 = {
+let init_p4 () = {
   player_name = "don";
   tiles = [];
   score = 0;
@@ -41,12 +41,10 @@ let p4 = {
   ai = true
 }
 
-let grid_w_hello = Grid.place
-
-let st = {
+let init_st () = {
   name = "game";
   grid = Grid.empty;
-  players = [p1;p2;p3;p4];
+  players = [init_p1 ();init_p2 ();init_p3 ();init_p4 ()];
   remaining_tiles = ['a';'b';'c';'d';'e';'f';'g';'h';'i'];
   turn = 0;
   score_history = [0;0;0;0;0;0]
@@ -69,34 +67,50 @@ let create_game_tests = [
       (create_game "user" "game" |> validate_cg));
 ]
 
-let add_result = add_player st "joe"
-
+let st1 = init_st ()
+let add_result = add_player st1 "joe"
 let add_player_tests = [
   "replaced first ai" >:: (fun _ -> assert_equal
     2
     add_result);
   "no longer ai" >:: (fun _ -> assert_equal
     false
-    ((List.nth (st.players) 2).ai));
+    ((List.nth (st1.players) 2).ai));
   "same order" >:: (fun _ -> assert_equal
-    p3.order
-    ((List.nth (st.players) 2).order));
+    (init_p3 ()).order
+    ((List.nth (st1.players) 2).order));
   "same tiles" >:: (fun _ -> assert_equal
-    p3.tiles
-    ((List.nth (st.players) 2).tiles));
+    (init_p3 ()).tiles
+    ((List.nth (st1.players) 2).tiles));
   "same score" >:: (fun _ -> assert_equal
-    p3.score
-    ((List.nth (st.players) 2).score));
+    (init_p3 ()).score
+    ((List.nth (st1.players) 2).score));
   "add duplicate" >:: (fun _ -> assert_raises
     PlayerExists
-    (fun () -> add_player st "joe"));
+    (fun () -> add_player st1 "joe"));
   "game full" >:: (fun _ -> assert_raises
     Full
-    (fun () -> add_player {st with players = [p1;p2]} "jake"));
+    (fun () -> add_player {st1 with players = [init_p1 ();init_p2 ()]} "jake"));
 ]
 
+let st2 = init_st ()
+let (removed, removed_turn) = remove_player st2 "alan"
 let remove_player_tests = [
-
+  "removed alan" >:: (fun _ -> assert_equal
+    ("(AI)",0)
+    (String.sub removed ((String.length removed) - 4) 4, removed_turn));
+  "now ai" >:: (fun _ -> assert_equal
+    true
+    ((List.nth (st2.players) removed_turn).ai));
+  "same order" >:: (fun _ -> assert_equal
+    (init_p1 ()).order
+    ((List.nth (st2.players) removed_turn).order));
+  "same tiles" >:: (fun _ -> assert_equal
+    (init_p1 ()).tiles
+    ((List.nth (st2.players) removed_turn).tiles));
+  "same score" >:: (fun _ -> assert_equal
+    (init_p1 ()).score
+    ((List.nth (st2.players) removed_turn).score));
 ]
 
 let error_tests = [
@@ -114,21 +128,22 @@ let error_tests = [
     (fun () -> execute s {move with tiles_placed = tp4}));
 ]
 
+let st3 = init_st ()
 let is_over_tests = [
-  "6 zero score turns" >:: (fun _ -> assert_equal true (is_over st));
+  "6 zero score turns" >:: (fun _ -> assert_equal true (is_over st3));
   "empty tile rack" >:: (fun _ -> assert_equal
     true
-    (is_over {st with remaining_tiles = []; score_history = [1;1;1;1;1;1]}));
+    (is_over {st3 with remaining_tiles = []; score_history = [1;1;1;1;1;1]}));
   "not over 1" (* still have tiles remaining*) >:: (fun _ -> assert_equal
     false
-    (is_over {st with score_history = [1;1;1;1;1;1]}));
+    (is_over {st3 with score_history = [1;1;1;1;1;1]}));
   "not over 2" (* everyone still has tiles*) >:: (fun _ -> assert_equal
     false
-    (is_over {st with remaining_tiles = []; players = [p1;p2;p3]; score_history = [1;1;1;1;1;1]}));
+    (is_over {st3 with remaining_tiles = []; players = [init_p1 ();init_p2 ();init_p3 ()]; score_history = [1;1;1;1;1;1]}));
 ]
 
 let tests =
   "test suite for game"  >::: error_tests @ is_over_tests @ create_game_tests @
-  add_player_tests
+  add_player_tests @ remove_player_tests
 
 let _ = run_test_tt_main tests
